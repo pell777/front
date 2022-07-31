74423193756a2532ddbc844cb9e18435468cb55c
Islam <islamdarbishev01@gmail.com>
2022-07-31 16:09:41 +0300

    master

--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="FormMain">
-      <div class="center examplex">
-      <vs-navbar  text-white  :color="active" center-collapsed v-model="active">
+    <div class="center examplex">
+      <vs-navbar text-white :color="active" center-collapsed v-model="active">
         <template #left>
           <h2>ToDo PrO</h2>
         </template>
@@ -9,48 +9,44 @@
           Главная
         </vs-navbar-item>
         <vs-navbar-item :active="active == 'docs'" id="docs">
-          Друзья
+          Друзьяяя
         </vs-navbar-item>
+        
         <vs-navbar-item :active="active == 'components'" id="components">
           Настройки
         </vs-navbar-item>
-
         <template #right>
-          
-        <vs-tooltip bottom>
-        <vs-button flat >
-          <span class="material-symbols-outlined">
-logout
-</span>
-        </vs-button>
-        <template #tooltip>
-        Выход
-        </template>
-      </vs-tooltip>
+          <vs-tooltip bottom>
+            <vs-button flat>
+              <span class="material-symbols-outlined">logout</span>
+            </vs-button>
+            <template #tooltip> Выход </template>
+          </vs-tooltip>
         </template>
       </vs-navbar>
     </div>
     <p class="p1">1</p>
-    </div> 
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-     active: 'dark'
+    active: "dark",
   }),
 };
 </script>
 
 <style>
-body{background: rgb(202, 202, 202);}
-.p1{
+body {
+  background: rgb(202, 202, 202);
+}
+.p1 {
   background: wheat;
-  margin-top:60px;
+  margin-top: 60px;
 }
 .material-symbols-outlined {
   height: 25px;
   width: 25px;
 }
-
-</style></style>
+</style>
